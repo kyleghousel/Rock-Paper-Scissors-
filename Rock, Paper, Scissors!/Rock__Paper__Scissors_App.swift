579581a76fdce62581d63f74c9edde27048a613c
kyleghousel <69773129+kyleghousel@users.noreply.github.com>
2024-01-21 16:29:40 -0500

    Initial Commit

---
+++ Rock, Paper, Scissors!/Rock__Paper__Scissors_App.swift
@@ -0,0 +1,17 @@
+//
+//  Rock__Paper__Scissors_App.swift
+//  Rock, Paper, Scissors!
+//
+//  Created by Kyle Housel on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Rock__Paper__Scissors_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
